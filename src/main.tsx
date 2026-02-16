154fa384115ece1d13fc2c21af911fd9b76c642e
TheJoboy <72822438+TheJoboy@users.noreply.github.com>
2026-02-16 22:29:50 +0100

    Fix routing on refresh in static hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { registerSW } from 'virtual:pwa-register';
 import { App } from './app/App';
 import { ToastProvider } from './app/toast';
@@ -10,8 +10,8 @@ registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ToastProvider><App /></ToastProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
